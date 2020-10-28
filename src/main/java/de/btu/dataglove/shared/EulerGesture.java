a26d6ff9a3622a1e988664a46228843d7e1c72b8
Ronny <hargowins@gmail.com>
2020-10-28 10:01:43 +0100

    added new EulerGesture constructor

--- src/main/java/de/btu/dataglove/shared/EulerGesture.java
+++ src/main/java/de/btu/dataglove/shared/EulerGesture.java
@@ -32,4 +32,35 @@ public class EulerGesture {
         this.upperBoundTheta = upperBoundTheta;
         this.upperBoundPsi = upperBoundPsi;
     }
+
+    public EulerGesture(String name, int typeOfGesture, int algorithmUsedForCalculation,
+                        double[] algorithmParameters, double[] allLowerBoundaries, double[] allUpperBoundaries) {
+        this.name = name;
+        this.typeOfGesture = typeOfGesture;
+        this.algorithmUsedForCalculation = algorithmUsedForCalculation;
+        this.algorithmParameters = algorithmParameters;
+        lowerBoundPhi = extractFieldFromAllBoundariesArray(allLowerBoundaries, 0);
+        lowerBoundTheta = extractFieldFromAllBoundariesArray(allLowerBoundaries, 1);
+        lowerBoundPsi = extractFieldFromAllBoundariesArray(allLowerBoundaries, 2);
+        upperBoundPhi = extractFieldFromAllBoundariesArray(allUpperBoundaries, 0);
+        upperBoundTheta = extractFieldFromAllBoundariesArray(allUpperBoundaries, 1);
+        upperBoundPsi = extractFieldFromAllBoundariesArray(allUpperBoundaries, 2);
+    }
+
+    /*
+    returns the boundary values for a given field based on the field id
+    field ids:
+    0: boundPhi
+    1: boundTheta
+    2: boundPsi
+    */
+    private double[] extractFieldFromAllBoundariesArray(double[] allBoundaries, int fieldID) {
+        double[] result = new double[SharedConstants.NUMBER_OF_SENSORS];
+
+        int startPos = fieldID * SharedConstants.NUMBER_OF_SENSORS;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = allBoundaries[startPos + i];
+        }
+        return result;
+    }
 }
