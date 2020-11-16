b08225d541586e57dd5f4cb299cfb2a41b88eb68
Ronny <hargowins@gmail.com>
2020-11-16 14:53:38 +0100

    added reverse lookup to algorithms enum

--- src/main/java/de/btu/dataglove/shared/Algorithms.java
+++ src/main/java/de/btu/dataglove/shared/Algorithms.java
@@ -1,5 +1,8 @@
 package de.btu.dataglove.shared;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 represents algorithms used to calculate gestures
  */
@@ -9,12 +12,25 @@ public enum Algorithms {
 
     private final int value;
 
+    //reverse lookup for getting the corresponding algorithm from an int
+    private static final Map<Integer, Algorithms> lookup = new HashMap<>();
+
+    static {
+        for (Algorithms a : Algorithms.values()) {
+            lookup.put(a.toInt(), a);
+        }
+    }
+
     Algorithms(int value) {
         this.value = value;
     }
 
     public int toInt() {
         return value;
+    }
+
+    public static Algorithms get(int valueOfAlgorithm) {
+        return lookup.get(valueOfAlgorithm);
     }
 
     public int getNumberOfParameters() {
