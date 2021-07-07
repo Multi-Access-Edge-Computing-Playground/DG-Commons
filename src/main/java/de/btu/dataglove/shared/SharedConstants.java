80fea62dbf1f3ac1976d3f991e83da10358a3420
Ronny <hargowins@gmail.com>
2021-07-07 09:33:53 +0200

    Naive Bayes with euler diffs is now supported by DB

--- src/main/java/de/btu/dataglove/shared/SharedConstants.java
+++ src/main/java/de/btu/dataglove/shared/SharedConstants.java
@@ -30,6 +30,7 @@ public class SharedConstants {
     public static final String DB_TABLE_ROBOT_ACTION = "/data-glove/rest/v1/RobotAction";
     public static final String DB_TABLE_OBJECT_TRACKING = "/data-glove/rest/v1/ObjectTracking";
     public static final String DB_TABLE_GESTURE_NAIVE_BAYES = "/data-glove/rest/v1/GestureNaiveBayes";
+    public static final String DB_TABLE_GESTURE_NAIVE_BAYES_EULER_DIFFS = "/data-glove/rest/v1/GestureNaiveBayesEulerDiffs";
 
 
     //exceptions
--- src/main/java/de/btu/dataglove/shared/webservice/HttpConnectionManager.java
+++ src/main/java/de/btu/dataglove/shared/webservice/HttpConnectionManager.java
@@ -470,6 +470,9 @@ public class HttpConnectionManager {
         if (clazz.getTypeName().equals(GestureNaiveBayes.class.getTypeName())) {
             return SharedConstants.DB_TABLE_GESTURE_NAIVE_BAYES;
         }
+        if (clazz.getTypeName().equals(GestureNaiveBayesEulerDiffs.class.getTypeName())) {
+            return SharedConstants.DB_TABLE_GESTURE_NAIVE_BAYES_EULER_DIFFS;
+        }
 
         throw new AssertionError("class not supported by db: " + clazz);
     }
